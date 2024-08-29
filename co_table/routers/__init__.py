6ccde5cc2a4a2c66067c582165f94eda36c684ed
krittamet-rrt <krittamet.rrt@gmail.com>
2024-08-29 15:27:39 +0700

    refactor: init_router add room.router

--- co_table/routers/__init__.py
+++ co_table/routers/__init__.py
@@ -1,6 +1,7 @@
-from . import root, user, authentication
+from . import root, user, authentication, room
 
 def init_routers(app):
     app.include_router(root.router)
     app.include_router(user.router)
     app.include_router(authentication.router)
+    app.include_router(room.router)
