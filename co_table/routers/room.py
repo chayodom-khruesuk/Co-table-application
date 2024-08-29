2adc329bf4217c19b70dbf2b116a84eb03129709
krittamet-rrt <krittamet.rrt@gmail.com>
2024-08-29 15:24:12 +0700

    feat: add room router with crud method

---
+++ co_table/routers/room.py
@@ -0,0 +1,61 @@
+from fastapi import APIRouter, Depends, HTTPException
+from sqlmodel import select, func
+from sqlalchemy.ext.asyncio import AsyncSession
+from typing import Annotated
+from .. import models
+from .. import deps
+
+import math
+
+router = APIRouter(
+    prefix="/rooms",
+    tags=["rooms"],
+)
+
+SIZE_PER_PAGE = 50
+
+@router.post("/", response_model=models.Room)
+async def create_room(room: models.CreateRoom, current_user: Annotated[models.User, Depends(deps.get_current_user)], session: Annotated[AsyncSession, Depends(models.get_session)]) -> models.Room:
+  db_room = models.DBRoom.model_validate(room)
+  session.add(db_room)
+  await session.commit()
+  await session.refresh(db_room)
+  return models.Room.model_validate
+
+@router.get("/", response_model=list[models.Room])
+async def get_rooms(session: Annotated[AsyncSession, Depends(models.get_session)], page: int = 1) -> models.RoomList:
+  result = await session.exec(select(models.DBRoom).offset((page - 1) * SIZE_PER_PAGE).limit(SIZE_PER_PAGE))
+
+  db_rooms = result.all()
+  page_count = int(math.ceil((await session.exec(select(func.count(models.DBRoom.id)))).first() / SIZE_PER_PAGE))
+
+  return models.RoomList.model_validate(rooms= db_rooms, page=page, page_count=page_count, size_per_page=SIZE_PER_PAGE)
+
+
+@router.get("/{room_id}", response_model=models.Room)
+async def get_room(room_id: int, session: Annotated[AsyncSession, Depends(models.get_session)]) -> models.Room:
+  db_room = await session.get(models.DBRoom, room_id)
+  if db_room:
+    return models.Room.model_validate(db_room)
+  raise HTTPException(status_code=404, detail="Room not found")
+
+@router.put("/{room_id}", response_model=models.Room)
+async def update_room(room_id: int, room: models.UpdateRoom, current_user: Annotated[models.User, Depends(deps.get_current_user)], session: Annotated[AsyncSession, Depends(models.get_session)]) -> models.Room:
+  db_room = await session.get(models.DBRoom, room_id)
+  if db_room:
+    for key, value in room.dict().item():
+      setattr(db_room, key, value)
+    session.add(db_room)
+    await session.commit()
+    await session.refresh(db_room)
+    return models.Room.model_validate(db_room)
+  raise HTTPException(status_code=404, detail="Room not found")
+
+@router.delete("/{room_id}")
+async def delete_room(room_id: int, current_user: Annotated[models.User, Depends(deps.get_current_user)], session: Annotated[AsyncSession, Depends(models.get_session)]) -> dict:
+  db_room = await session.get(models.DBRoom, room_id)
+  if db_room:
+    await session.delete(db_room)
+    await session.commit()
+    return {"message": "Room deleted"}
+  raise HTTPException(status_code=404, detail="Room not found")
