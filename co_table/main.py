8903abb16edd096188205e6e1358d4279ab3f434
Chayodom-Khruesuk <chayodom.kx@gmail.com>
2024-10-15 02:27:06 +0700

    add: prepare for deploy

--- co_table/main.py
+++ co_table/main.py
@@ -4,21 +4,33 @@ from co_table import config
 
 from contextlib import asynccontextmanager
 
+from fastapi.middleware.cors import CORSMiddleware
+
 from . import models
 from . import routers
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
+    await models.create_all()
     yield
     if models.engine is not None:
         await models.close_session()
         await models.engine.dispose()
 
 def create_app(settings = None):
+    
     if not settings:
         settings = config.get_setting()
-
+    
     app = FastAPI(lifespan = lifespan)
+
+    app.add_middleware(
+        CORSMiddleware,
+        allow_origins=[""],
+        allow_credentials=True,
+        allow_methods=[""],
+        allow_headers=["*"],
+    )
 
     models.init_db(settings)
 
